f6e1a1caa85a3a69783d5d28c73803e60edefdab
zdenislava <navarikova@seznam.cz>
2021-04-06 19:01:44 +0200

    main functionality

--- main.js
+++ main.js
@@ -35,11 +35,21 @@ function tahnoutJmeno() {
     }
 
     // Generujeme náhodný index
+    let vyherniIndex = Math.floor(Math.random() * jmena.length);
+    console.log(vyherniIndex);
 
     // Získáme výherní jméno na patřičném indexu
+    let vyherniJmeno = jmena[vyherniIndex];
+    console.log(vyherniJmeno);
 
+    
+
+    let vyherce = document.querySelector("#vyherka");
+    vyherce.innerHTML = vyherniJmeno;
+    
     // Vyřadíme vylosované jméno z osudí
 
     // Výherní jméno si uložíme do pole k ostatním výherním
+
     
 }
